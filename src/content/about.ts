a03a4dfa38e7e4c96e1a62e988bb500d6b6cadfd
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2025-10-31 12:06:09 +0100

    - Changed Layout of the Polish About section.

--- src/content/about.ts
+++ src/content/about.ts
@@ -95,11 +95,15 @@ export const aboutContent: Record<'en' | 'pl', { sections: AboutSection[] }> = {
       {
         title: 'Pomóż budować bazę danych!',
         paragraphs: [
-          'Istnieje kilka zespołów danych stanowiących fascynujący materiał źródłowy na temat geografii ekonomicznej II RP:',
-          ' - Dane na temat stacjonowania armii - rekonstrukcja na temat źródeł wojskowych, nie pojawiają się w spisach. [materiał na pracę magisterską]',
-          ' - Dane na temat elektrowni i zakładów przemysłowych pobierających prąd - jedne z geograficznie najbardziej szczegółowych danych na temat II RP publikowane w Statystyce Zakładów Elektrycznych w Polsce (1925-1936). [materiał na pracę magisterską]',
-          ' - Dane na temat infrastruktury w miastach (wyposażenia w instalacje elektryczne i gazowe, kanalizację, wodociąg, materiał konstrukcyjny ścian i dachów). [materiał na licencjat]',
-          ' - Dane na temat wielkiej własności rolnej w 1921 (wielkość i użytkowanie gruntu w wielkiej własności prywatnej, kościelnej i publicznej + szczegółowy spis posiadanych maszyn rolniczych i zakładów produkcji rolnej). [materiał na licencjat lub pracę magisterską]',
+          'Istnieje kilka zespołów danych stanowiących fascynujący materiał źródłowy na temat geografii ekonomicznej II RP:'
+        ],
+        bullets: [
+          'Dane na temat stacjonowania armii - rekonstrukcja na temat źródeł wojskowych, nie pojawiają się w spisach. [materiał na pracę magisterską]',
+          'Dane na temat elektrowni i zakładów przemysłowych pobierających prąd - jedne z geograficznie najbardziej szczegółowych danych na temat II RP publikowane w Statystyce Zakładów Elektrycznych w Polsce (1925-1936). [materiał na pracę magisterską]',
+          'Dane na temat infrastruktury w miastach (wyposażenia w instalacje elektryczne i gazowe, kanalizację, wodociąg, materiał konstrukcyjny ścian i dachów). [materiał na licencjat]',
+          'Dane na temat wielkiej własności rolnej w 1921 (wielkość i użytkowanie gruntu w wielkiej własności prywatnej, kościelnej i publicznej + szczegółowy spis posiadanych maszyn rolniczych i zakładów produkcji rolnej). [materiał na licencjat lub pracę magisterską]'
+        ],
+        paragraphs: [
           'Jeśli fascynujesz się historią i chcesz wesprzeć wysiłek budowy tej bazy dzięki digitalizacji (w czasie wolnym czy w ramach swojej pracy naukowej), oferuję z mojej strony wsparcie w doborze najlepszych narzędzi cyfrowych do digitalizacji i wsparcie w opracowaniu źródeł.'
         ]
       },
